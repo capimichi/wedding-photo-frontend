c830339b4c811f21ec7499a23708c7dc02cff025
Michele Capichioni <capimichi@gmail.com>
2025-06-19 14:38:23 +0200

    feat: add legacy support for older browsers with @vitejs/plugin-legacy
    - Updated package.json to include @vitejs/plugin-legacy version 6.1.1.
    - Modified vite.config.ts to configure legacy plugin with specified targets and polyfills.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import legacy from '@vitejs/plugin-legacy'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11', 'iOS >= 10', 'Safari >= 10'],
+      polyfills: true,
+      modernPolyfills: true,
+    }),
   ],
   resolve: {
     alias: {
